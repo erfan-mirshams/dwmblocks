15297b6752981d623f0c9670ee0fbd1c316775cd
Erfan Mirshams <erfanmirshams@protonmail.com>
2021-09-16 23:24:21 +0430

    Add internet and memory

--- blocks.def.h
+++ blocks.def.h
@@ -1,10 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
- 	 {"", "sb-keyboard",    0,      1},
+ 	 {"", "sb-memory", 60, 0},
+ 	 {"", "sb-keyboard", 0, 1},
 	 {"", "sb-date", 60, 0},
 	 {"", "sb-clock", 60, 0},
 	 {"", "sb-volume", 0, 2},
+	 {"", "sb-internet", 60, 0},
 	 {"", "sb-battery", 60, 0}
 };
 
